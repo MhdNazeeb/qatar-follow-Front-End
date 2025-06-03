2e3e3b645beadced67049563cf9328a5abd755a0
Muhammed Nazeeb <naseebn819@gmail.com>
2025-06-03 20:39:08 +0300

    production-5

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,9 @@ module.exports = {
       'plugin:@typescript-eslint/recommended',
     ],
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+      },
+    
   };
   
